596f5a9b822f4d84749528c34fac7bb02cddf7bc
Peter Ledbrook <peter@cacoethes.co.uk>
2009-10-27 19:32:14 +0000

    The Java 1.5 compiler doesn't like the @Override annotation, so it's been removed from DummySecurityManager.

--- src/java/org/apache/shiro/grails/DummySecurityManager.java
+++ src/java/org/apache/shiro/grails/DummySecurityManager.java
@@ -25,74 +25,39 @@ import org.apache.shiro.subject.Subject;
  * @author Peter Ledbrook
  */
 public class DummySecurityManager implements SecurityManager {
-	@Override
-	public Subject createSubject(Map arg0) { return null; }
-	@Override
-	public Subject getSubject() { return null; }
-	@Override
-	public Subject login(Subject arg0, AuthenticationToken arg1) throws AuthenticationException { return null; }
-	@Override
-	public void logout(Subject arg0) { }
-	@Override
-	public AuthenticationInfo authenticate(AuthenticationToken arg0) throws AuthenticationException { return null; }
-	@Override
-	public void checkPermission(PrincipalCollection arg0, String arg1) throws AuthorizationException { }
-	@Override
-	public void checkPermission(PrincipalCollection arg0, Permission arg1) throws AuthorizationException { }
-	@Override
-	public void checkPermissions(PrincipalCollection arg0, String... arg1) throws AuthorizationException { }
-	@Override
-	public void checkPermissions(PrincipalCollection arg0, Collection<Permission> arg1) throws AuthorizationException { }
-	@Override
-	public void checkRole(PrincipalCollection arg0, String arg1) throws AuthorizationException { }
-	@Override
-	public void checkRoles(PrincipalCollection arg0, Collection<String> arg1) throws AuthorizationException { }
-	@Override
-	public boolean hasAllRoles(PrincipalCollection arg0, Collection<String> arg1) { return false; }
-	@Override
-	public boolean hasRole(PrincipalCollection arg0, String arg1) { return false; }
-	@Override
-	public boolean[] hasRoles(PrincipalCollection arg0, List<String> arg1) { return null; }
-	@Override
-	public boolean isPermitted(PrincipalCollection arg0, String arg1) { return false; }
-	@Override
-	public boolean isPermitted(PrincipalCollection arg0, Permission arg1) { return false; }
-	@Override
-	public boolean[] isPermitted(PrincipalCollection arg0, String... arg1) { return null; }
-	@Override
-	public boolean[] isPermitted(PrincipalCollection arg0, List<Permission> arg1) { return null; }
-	@Override
-	public boolean isPermittedAll(PrincipalCollection arg0, String... arg1) { return false; }
-	@Override
-	public boolean isPermittedAll(PrincipalCollection arg0, Collection<Permission> arg1) { return false; }
-	@Override
-	public void checkValid(Serializable arg0) throws InvalidSessionException { } 
-	@Override
-	public Object getAttribute(Serializable arg0, Object arg1) throws InvalidSessionException { return null; }
-	@Override
-	public Collection<Object> getAttributeKeys(Serializable arg0) { return null; }
-	@Override
-	public InetAddress getHostAddress(Serializable arg0) { return null; }
-	@Override
-	public Date getLastAccessTime(Serializable arg0) { return null; }
-	@Override
-	public Date getStartTimestamp(Serializable arg0) { return null; }
-	@Override
-	public long getTimeout(Serializable arg0) throws InvalidSessionException { return 0; }
-	@Override
-	public boolean isValid(Serializable arg0) { return false; }
-	@Override
-	public Object removeAttribute(Serializable arg0, Object arg1) throws InvalidSessionException { return null; }
-	@Override
-	public void setAttribute(Serializable arg0, Object arg1, Object arg2) throws InvalidSessionException { }
-	@Override
-	public void setTimeout(Serializable arg0, long arg1) throws InvalidSessionException { }
-	@Override
-	public Serializable start(InetAddress arg0) throws AuthorizationException { return null; }
-	@Override
-	public Serializable start(Map arg0) throws AuthorizationException { return null; }
-	@Override
-	public void stop(Serializable arg0) throws InvalidSessionException { }
-	@Override
-	public void touch(Serializable arg0) throws InvalidSessionException { }
+    public Subject createSubject(Map arg0) { return null; }
+    public Subject getSubject() { return null; }
+    public Subject login(Subject arg0, AuthenticationToken arg1) throws AuthenticationException { return null; }
+    public void logout(Subject arg0) { }
+    public AuthenticationInfo authenticate(AuthenticationToken arg0) throws AuthenticationException { return null; }
+    public void checkPermission(PrincipalCollection arg0, String arg1) throws AuthorizationException { }
+    public void checkPermission(PrincipalCollection arg0, Permission arg1) throws AuthorizationException { }
+    public void checkPermissions(PrincipalCollection arg0, String... arg1) throws AuthorizationException { }
+    public void checkPermissions(PrincipalCollection arg0, Collection<Permission> arg1) throws AuthorizationException { }
+    public void checkRole(PrincipalCollection arg0, String arg1) throws AuthorizationException { }
+    public void checkRoles(PrincipalCollection arg0, Collection<String> arg1) throws AuthorizationException { }
+    public boolean hasAllRoles(PrincipalCollection arg0, Collection<String> arg1) { return false; }
+    public boolean hasRole(PrincipalCollection arg0, String arg1) { return false; }
+    public boolean[] hasRoles(PrincipalCollection arg0, List<String> arg1) { return null; }
+    public boolean isPermitted(PrincipalCollection arg0, String arg1) { return false; }
+    public boolean isPermitted(PrincipalCollection arg0, Permission arg1) { return false; }
+    public boolean[] isPermitted(PrincipalCollection arg0, String... arg1) { return null; }
+    public boolean[] isPermitted(PrincipalCollection arg0, List<Permission> arg1) { return null; }
+    public boolean isPermittedAll(PrincipalCollection arg0, String... arg1) { return false; }
+    public boolean isPermittedAll(PrincipalCollection arg0, Collection<Permission> arg1) { return false; }
+    public void checkValid(Serializable arg0) throws InvalidSessionException { } 
+    public Object getAttribute(Serializable arg0, Object arg1) throws InvalidSessionException { return null; }
+    public Collection<Object> getAttributeKeys(Serializable arg0) { return null; }
+    public InetAddress getHostAddress(Serializable arg0) { return null; }
+    public Date getLastAccessTime(Serializable arg0) { return null; }
+    public Date getStartTimestamp(Serializable arg0) { return null; }
+    public long getTimeout(Serializable arg0) throws InvalidSessionException { return 0; }
+    public boolean isValid(Serializable arg0) { return false; }
+    public Object removeAttribute(Serializable arg0, Object arg1) throws InvalidSessionException { return null; }
+    public void setAttribute(Serializable arg0, Object arg1, Object arg2) throws InvalidSessionException { }
+    public void setTimeout(Serializable arg0, long arg1) throws InvalidSessionException { }
+    public Serializable start(InetAddress arg0) throws AuthorizationException { return null; }
+    public Serializable start(Map arg0) throws AuthorizationException { return null; }
+    public void stop(Serializable arg0) throws InvalidSessionException { }
+    public void touch(Serializable arg0) throws InvalidSessionException { }
 }
