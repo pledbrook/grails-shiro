8f99ec231e060b4ab411dec54e1dd463033697d8
pmcneil <peter@mcneils.net>
2013-09-17 23:01:26 +1000

    Update cli-tests project - it works!

--- test/projects/cli-tests/grails-app/conf/BuildConfig.groovy
+++ test/projects/cli-tests/grails-app/conf/BuildConfig.groovy
@@ -1,33 +1,37 @@
-grails.plugin.location.shiro = "../../.."
-
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+grails.plugin.location.shiro = "../../.."
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
-        // uncomment to disable ehcache
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
     repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
         grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        mavenLocal()
+        mavenCentral()
 
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
+
+    dependencies {
+    }
+
+    plugins {
+        runtime ":hibernate:$grailsVersion"
     }
 }
