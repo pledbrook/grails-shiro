0fe55c749c07b833d9bbd0b5bda8ee561e5daa0c
pmcneil <peter@mcneils.net>
2013-09-30 22:00:13 +1000

    Update build config. Use the specific dependencies instead of shiro-all as this http://shiro.apache.org/download.html says don't use all. Also remove the "optional" part for dependencies and include shiro-quartz -cas and -ehcache so people can use them. Remove the grails.poject.dependency.distribution as we're now using the release plugin and we also don't want to show our local deploy credentials

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,6 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
     inherits "global"
@@ -13,10 +12,11 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile 'org.apache.shiro:shiro-all:1.2.2'
-
-        optional 'org.apache.shiro:shiro-ehcache:1.2.2',
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes
+        compile 'org.apache.shiro:shiro-core:1.2.2',
+                'org.apache.shiro:shiro-web:1.2.2',
+                'org.apache.shiro:shiro-spring:1.2.2',
+                'org.apache.shiro:shiro-ehcache:1.2.2',
                 'org.apache.shiro:shiro-quartz:1.2.2',
                 'org.apache.shiro:shiro-cas:1.2.2', {
             excludes 'ejb', 'jsf-api', 'jms', 'connector-api', 'ehcache-core', 'slf4j-api', 'commons-logging'
@@ -32,13 +32,4 @@ grails.project.dependency.resolution = {
     }
 }
 
-grails.project.dependency.distribution = {
-    portal id: "beta", url: "http://beta.grails.org/"
-
-    remoteRepository(id: "snapshots", url: "http://myserver:8081/artifactory/libs-snapshots-local")
-
-    remoteRepository(id: "releases", url: "http://myserver:8081/artifactory/libs-releases-local") {
-        authentication username: "admin", password: "password"
-    }
-}
 
