566ad6bb86e5d904cfbcb3beb7a56d51dfe0051c
Burt Beckwith <burt@burtbeckwith.com>
2014-03-05 23:45:49 -0500

    don't export the hibernate or rest-client-builder plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,9 +23,11 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        build (":tomcat:$grailsVersion",
-                ":release:2.2.1") {
+        runtime ":hibernate:$grailsVersion", {
+            export = false
+        }
+
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
             export = false
         }
     }
