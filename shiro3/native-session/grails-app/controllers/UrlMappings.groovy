600b2a5d3963f532847a815f60bbef802cf4cf8c
Yellowsnow <melaoufir@gmail.com>
2015-10-23 18:07:47 +0100

    Issue #44 Work in progress on native-session tests (Had to disable legacy configuration)

--- shiro3/native-session/grails-app/controllers/UrlMappings.groovy
+++ shiro3/native-session/grails-app/controllers/UrlMappings.groovy
@@ -5,7 +5,11 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-      
+
+        "/auth/signIn"(controller: "auth", action: "signIn")
+        "/login"(controller: "auth", action: "login")
+        "/unauthorized"(controller: "auth", action: "unauthorized")
+        "/login"(controller: "auth", action: "login")
         "/"(view:"/index")
         "500"(view:'/error')
 
